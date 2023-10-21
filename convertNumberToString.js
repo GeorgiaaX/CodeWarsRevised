4e562b5d3e9b45040d523dd6dffa193bf2d4bf1d
GeorgiaaX <georgiaa.devv@gmail.com>
2023-10-21 10:08:33 +1000

    final

---
+++ convertNumberToString.js
@@ -0,0 +1,12 @@
+// We need a function that can transform a number (integer) into a string.
+
+//give an interger of any length, always an integer
+//return the integer as a string
+
+function numToStr (num) {
+    return num.toString()
+}
+
+// console.log(numToStr(123), "123")
+// console.log(numToStr(-65), "-65")
+// console.log(numToStr(0), "0")
