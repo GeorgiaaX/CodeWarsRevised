7027d0776e8cbe535102b03fe70b7bcbafa50603
GeorgiaaX <georgiaa.devv@gmail.com>
2023-10-22 19:25:23 +1000

    final

---
+++ oppostieNumber.js
@@ -0,0 +1,12 @@
+// given an integer or a floating-point number, find its opposite.
+
+//number or integer given, either a positive or negative value, only a number
+//return the opposite postive or negative value as an integer
+
+function oppositeNumber(num) {
+    return num * -1
+}
+
+console.log(oppositeNumber(1), -1)
+console.log(oppositeNumber(-239), 239)
+console.log(oppositeNumber(3.14), -3.14)
