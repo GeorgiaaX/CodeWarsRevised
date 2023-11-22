47a32855962834398f1e5046e7fc19596b145622
GeorgiaaX <georgiaa.devv@gmail.com>
2023-11-22 21:23:47 +1000

    final

---
+++ returningStrings.js
@@ -0,0 +1,13 @@
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?"
+
+//takes an input
+
+//returns the phrase "Hello <name> how are you doing today"
+
+function personalisedGreeting(name) {
+    alert(`Hello ${name}, how are you today?`)
+}
+
+console.log(personalisedGreeting("Greg"), "Hello Greg, how are you today?")
+console.log(personalisedGreeting("Mary"), "Hello Mary, how are you today?")
+console.log(personalisedGreeting("Sarah"), "Hello Sarah, how are you today?")
